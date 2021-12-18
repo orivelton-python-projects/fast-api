7c9ca6bb72ab0aade76fd77f52d6fd11318686b7
Orivelton Cesar <orivelton10@gmail.com>
2021-12-18 15:26:19 +0000

    chore: Initial router

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
 @app.get("/")
-async def root():
-  return {"message": "Hello World"}
+def root():
+  return { "message": "Hello World" }
+
+@app.get("/posts")
+def get_posts():
+  return { "data": "This is a post"}
+
+@app.post("/createposts")
+def create_posts():
+  return { "message": "Succesfully creates posts"}
